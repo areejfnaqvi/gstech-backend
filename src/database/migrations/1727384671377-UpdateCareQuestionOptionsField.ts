bc914c6a5a343d806d05e0cf492e2316d82056a6
Areej Naqvi <areej.faisal@conradlabs.com>
2024-09-28 13:51:41 +0500

    fix: resolve migrations errors

--- src/database/migrations/1727384671377-UpdateCareQuestionOptionsField.ts
+++ src/database/migrations/1727384671377-UpdateCareQuestionOptionsField.ts
@@ -12,15 +12,9 @@ export class UpdateCareQuestionOptionsField1727384671377
         await queryRunner.query(
             `ALTER TABLE "care_question_options" ADD "questionID" integer NOT NULL`,
         );
-        await queryRunner.query(
-            `ALTER TABLE "care_question_options" ADD CONSTRAINT "FK_219e06b1f48069fdccd53012804" FOREIGN KEY ("questionID") REFERENCES "care_questions"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
-        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(
-            `ALTER TABLE "care_question_options" DROP CONSTRAINT "FK_219e06b1f48069fdccd53012804"`,
-        );
         await queryRunner.query(
             `ALTER TABLE "care_question_options" DROP COLUMN "questionID"`,
         );
--- src/database/migrations/1727423689154-UpdateCareQuestionOptionsRelation.ts
+++ src/database/migrations/1727423689154-UpdateCareQuestionOptionsRelation.ts
@@ -7,25 +7,13 @@ export class UpdateCareQuestionOptionsRelation1727423689154
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `ALTER TABLE "care_question_options" DROP CONSTRAINT "FK_219e06b1f48069fdccd53012804"`,
-        );
-        await queryRunner.query(
             `ALTER TABLE "care_question_options" ALTER COLUMN "questionID" DROP NOT NULL`,
-        );
-        await queryRunner.query(
-            `ALTER TABLE "care_question_options" ADD CONSTRAINT "FK_219e06b1f48069fdccd53012804" FOREIGN KEY ("questionID") REFERENCES "care_questions"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
         );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `ALTER TABLE "care_question_options" DROP CONSTRAINT "FK_219e06b1f48069fdccd53012804"`,
-        );
-        await queryRunner.query(
             `ALTER TABLE "care_question_options" ALTER COLUMN "questionID" SET NOT NULL`,
-        );
-        await queryRunner.query(
-            `ALTER TABLE "care_question_options" ADD CONSTRAINT "FK_219e06b1f48069fdccd53012804" FOREIGN KEY ("questionID") REFERENCES "care_questions"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
         );
     }
 }

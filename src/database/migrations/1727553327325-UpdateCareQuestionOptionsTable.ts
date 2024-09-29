5370297756f51cac5d3f3e9bffd317b2b7f8df04
Areej Naqvi <areej.faisal@conradlabs.com>
2024-09-30 02:18:07 +0500

    fix: fix migrations error

--- src/database/migrations/1727553327325-UpdateCareQuestionOptionsTable.ts
+++ src/database/migrations/1727553327325-UpdateCareQuestionOptionsTable.ts
@@ -4,7 +4,6 @@ export class UpdateCareQuestionOptionsTable1727553327325 implements MigrationInt
     name = 'UpdateCareQuestionOptionsTable1727553327325'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "care_question_options" DROP CONSTRAINT "FK_219e06b1f48069fdccd53012804"`);
         await queryRunner.query(`ALTER TABLE "care_question_options" RENAME COLUMN "questionID" TO "optionID"`);
         await queryRunner.query(`CREATE TABLE "question_options" ("questionID" integer NOT NULL, "optionID" integer NOT NULL, CONSTRAINT "PK_fc2d27ad3008ba9c052007a66a2" PRIMARY KEY ("questionID", "optionID"))`);
         await queryRunner.query(`CREATE INDEX "IDX_1fc095d202e9347e69279a5643" ON "question_options" ("questionID") `);
@@ -12,13 +11,9 @@ export class UpdateCareQuestionOptionsTable1727553327325 implements MigrationInt
         await queryRunner.query(`ALTER TABLE "care_question_options" ALTER COLUMN "optionID" SET NOT NULL`);
         await queryRunner.query(`ALTER TABLE "care_questions" DROP COLUMN "dependentOn"`);
         await queryRunner.query(`ALTER TABLE "care_questions" ADD "dependentOn" text array`);
-        await queryRunner.query(`ALTER TABLE "question_options" ADD CONSTRAINT "FK_1fc095d202e9347e69279a5643e" FOREIGN KEY ("questionID") REFERENCES "care_questions"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
-        await queryRunner.query(`ALTER TABLE "question_options" ADD CONSTRAINT "FK_d693867784c85431d2b0c4b2604" FOREIGN KEY ("optionID") REFERENCES "care_question_options"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "question_options" DROP CONSTRAINT "FK_d693867784c85431d2b0c4b2604"`);
-        await queryRunner.query(`ALTER TABLE "question_options" DROP CONSTRAINT "FK_1fc095d202e9347e69279a5643e"`);
         await queryRunner.query(`ALTER TABLE "care_questions" DROP COLUMN "dependentOn"`);
         await queryRunner.query(`ALTER TABLE "care_questions" ADD "dependentOn" jsonb`);
         await queryRunner.query(`ALTER TABLE "care_question_options" ALTER COLUMN "optionID" DROP NOT NULL`);
@@ -26,7 +21,6 @@ export class UpdateCareQuestionOptionsTable1727553327325 implements MigrationInt
         await queryRunner.query(`DROP INDEX "public"."IDX_1fc095d202e9347e69279a5643"`);
         await queryRunner.query(`DROP TABLE "question_options"`);
         await queryRunner.query(`ALTER TABLE "care_question_options" RENAME COLUMN "optionID" TO "questionID"`);
-        await queryRunner.query(`ALTER TABLE "care_question_options" ADD CONSTRAINT "FK_219e06b1f48069fdccd53012804" FOREIGN KEY ("questionID") REFERENCES "care_questions"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
 }
--- src/database/migrations/1727604552698-UpdateCareQuestionOptionsTable.ts
+++ src/database/migrations/1727604552698-UpdateCareQuestionOptionsTable.ts
@@ -7,13 +7,11 @@ export class UpdateCareQuestionOptionsTable1727604552698 implements MigrationInt
         await queryRunner.query(`CREATE TABLE "care_question_option_relations" ("questionID" integer NOT NULL, "optionID" integer NOT NULL, CONSTRAINT "PK_95eabe838e3639112657f3327f3" PRIMARY KEY ("questionID", "optionID"))`);
         await queryRunner.query(`CREATE INDEX "IDX_e74e3c2bec29dfa990c32eb3e0" ON "care_question_option_relations" ("questionID") `);
         await queryRunner.query(`CREATE INDEX "IDX_f584930be271502e3677f4b1b4" ON "care_question_option_relations" ("optionID") `);
-        await queryRunner.query(`ALTER TABLE "care_question_option_relations" ADD CONSTRAINT "FK_e74e3c2bec29dfa990c32eb3e00" FOREIGN KEY ("questionID") REFERENCES "care_questions"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
         await queryRunner.query(`ALTER TABLE "care_question_option_relations" ADD CONSTRAINT "FK_f584930be271502e3677f4b1b4f" FOREIGN KEY ("optionID") REFERENCES "care_question_options"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "care_question_option_relations" DROP CONSTRAINT "FK_f584930be271502e3677f4b1b4f"`);
-        await queryRunner.query(`ALTER TABLE "care_question_option_relations" DROP CONSTRAINT "FK_e74e3c2bec29dfa990c32eb3e00"`);
         await queryRunner.query(`DROP INDEX "public"."IDX_f584930be271502e3677f4b1b4"`);
         await queryRunner.query(`DROP INDEX "public"."IDX_e74e3c2bec29dfa990c32eb3e0"`);
         await queryRunner.query(`DROP TABLE "care_question_option_relations"`);

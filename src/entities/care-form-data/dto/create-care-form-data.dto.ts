d92667413392d7ec44e58ecd642fe63cb1cfc913
Areej Naqvi <areej.faisal@conradlabs.com>
2024-09-30 17:03:08 +0500

    fix: remove length check for ssn

--- src/entities/care-form-data/dto/create-care-form-data.dto.ts
+++ src/entities/care-form-data/dto/create-care-form-data.dto.ts
@@ -13,8 +13,5 @@ export class CreateCareFormDataDto {
 
     @ApiProperty({ type: Number })
     @IsNumber()
-    @Length(9, 9, {
-        message: 'The length of SSNs must be exactly 9 characters',
-    })
     '3.3.5': number;
 }
